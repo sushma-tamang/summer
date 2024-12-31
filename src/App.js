3bfe1e8b81a9bc7de6a80ad95ea95475d19d0c6d
sushma-tamang <ghisingsushma905@gmail.com>
2024-12-31 21:11:13 +0545

    change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="bg-green-500 rounded-full h-6 ">
 
     <h1 className='text-white text-left px-10 bg-black my-9 mx-9'>
-      hello world hey
+      hello world hey sushma Lord loves you believe in him
     </h1>
     <p className='text-white px-5 bg-black my-3 mx-3'>
     Mental health refers to a person’s emotional, psychological, and social well-being. It affects how individuals think, feel, and behave in daily life. Mental health also determines how we handle stress, relate to others, and make choices. It's a crucial aspect of overall health and is often influenced by a variety of factors, including biological, environmental, and social conditions.
